e828b15f58abc1cbb41416e8625251c4ab25e999
Jonathan Brown <jbrown@link-blockchain.org>
2017-10-05 16:06:18 +0700

    Store payload in IPFS.

--- frontend.js
+++ frontend.js
@@ -42,23 +42,54 @@ $(function() {
     var base64Container = base64js.fromByteArray(containerPayload);
     console.log(base64Container);
 
+    var data = '--4na9stc9e6a\nContent-Disposition: file; filename=""\nContent-Type: application/octet-stream\n\n' + Buffer.from(containerPayload).toString('binary'); + '\n--4na9stc9e6a--';
+    
+    console.log(data);
+
+    $.ajax({
+      url: "http://127.0.0.1:5001/api/v0/add",
+      method: "POST",
+      data: data,
+      cache: false,
+      processData: false, // Don't process the files
+      contentType: 'multipart/form-data; boundary=4na9stc9e6a',
+      mimeType: "application/json",
+      dataType: "json"
+    })
+      .done(function(result) {
+        var hash = result.Hash;
+        
+        console.log(hash);
 
     // Decode
 
-    var containerMessage2 = container.ItemContainer.deserializeBinary(base64js.toByteArray(base64Container));
-    var itemPayloadCompressed2 = containerMessage2.getPayload();
-    console.log(itemPayloadCompressed2);
+      $.ajax({
+        url: "http://127.0.0.1:5001/api/v0/cat?arg=/ipfs/" + hash,
+        method: "GET",
+      })
+        .done(function(result) {
+        
+          console.log(result);
+          console.log(result.length);
+          
+          var containerPayload2 = new Uint8Array(Buffer.from(result, "binary"));
+          console.log(containerPayload2);
+        
+          var containerMessage2 = container.ItemContainer.deserializeBinary(containerPayload2);
+          var itemPayloadCompressed2 = containerMessage2.getPayload();
+          console.log(itemPayloadCompressed2);
 
-    var itemMessage2 = container.Item.deserializeBinary(pako.inflateRaw(itemPayloadCompressed2));
-    var mixins = itemMessage2.getMixinsList();
-    
-    var mixinPayload2 = mixins[0].getPayload();
-    console.log(mixinPayload2);
-    
-    var pastbinMixinMessage2 = pastebinmixin.PastebinMixin.deserializeBinary(mixinPayload2);
-    
-    console.log(pastbinMixinMessage2.getText());
-    
+          var itemMessage2 = container.Item.deserializeBinary(pako.inflateRaw(itemPayloadCompressed2));
+          var mixins = itemMessage2.getMixinsList();
+          
+          var mixinPayload2 = mixins[0].getPayload();
+          console.log(mixinPayload2);
+          
+          var pastbinMixinMessage2 = pastebinmixin.PastebinMixin.deserializeBinary(mixinPayload2);
+          
+          console.log(pastbinMixinMessage2.getText());
+        });
+      });
   });
 
 });
